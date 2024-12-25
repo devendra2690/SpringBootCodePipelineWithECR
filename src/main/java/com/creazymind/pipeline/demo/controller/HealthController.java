3bd78f1718af27904c4746d545aa8cf349417eed
Devendra Patil <devendra_patil26@yahoo.com>
2024-12-25 16:52:22 +0530

    Adding health check

---
+++ src/main/java/com/creazymind/pipeline/demo/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.creazymind.pipeline.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "App is up";
+    }
+}
